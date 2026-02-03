2564dac73e212d89f622ea666e050f757078e885
vanedelsaltos <vanedelsaltos@gmail.com>
2026-02-02 23:57:10 -0500

    Update settings.py

--- settings 2-para render/settings.py
+++ settings 2-para render/settings.py
@@ -36,7 +36,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # <-- ahora lee del .env y l
 #   'ALLOWED_HOSTS',
 #    '127.0.0.1,localhost'
 #).split(',')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
